5759bd9f65cef0342daed4f41b60b98711a1aa17
sanyagribanov <K>
2023-03-15 11:10:49 +0300

    first commit

---
+++ app/src/main/java/com/example/randomdog/models/api/ApiRequest.kt
@@ -0,0 +1,10 @@
+package com.example.randomdog.models.api
+import com.example.randomdog.models.Dog
+import retrofit2.http.GET
+
+const val BASE_URL = "https://random.dog/"
+
+interface ApiRequest {
+    @GET("woof.json")
+    suspend fun getRandomDog(): Dog
+}
